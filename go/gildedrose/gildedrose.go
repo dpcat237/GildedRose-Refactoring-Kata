69795a2240c591258c7e4927224c254c394fa201
Denys Pasishnyi <dpcat237@gmail.com>
2023-07-14 11:56:18 +0200

    Simplify UpdateQuality method

--- go/gildedrose/gildedrose.go
+++ go/gildedrose/gildedrose.go
@@ -7,52 +7,57 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
+		updateItemQuality(items[i])
+	}
+}
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
-			}
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 6 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
-		}
+func (i *Item) decreaseQuality(q int) {
+	if i.Quality > 0 {
+		i.Quality = i.Quality - q
+	}
+}
 
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
-		}
+func (i *Item) decreaseSellIn(s int) {
+	i.SellIn = i.SellIn - s
+}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name != "Aged Brie" {
-				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
-					}
-				} else {
-					items[i].Quality = items[i].Quality - items[i].Quality
-				}
-			} else {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			}
-		}
+func (i *Item) increaseQuality(q int) {
+	if i.Quality < 50 {
+		i.Quality = i.Quality + q
+	}
+}
+
+func updateItemQuality(item *Item) {
+	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
+		item.Name != "Sulfuras, Hand of Ragnaros" {
+		item.decreaseQuality(1)
+	}
+	if item.Name == "Aged Brie" || item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		item.increaseQuality(1)
+	}
+	if item.SellIn < 11 && item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		item.increaseQuality(1)
+	}
+	if item.SellIn < 6 && item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		item.increaseQuality(1)
+	}
+	if item.Name != "Sulfuras, Hand of Ragnaros" {
+		item.decreaseSellIn(1)
 	}
 
+	if item.SellIn >= 0 {
+		return
+	}
+	if item.Name == "Aged Brie" {
+		item.increaseQuality(1)
+		return
+	}
+	if item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
+		item.Name != "Sulfuras, Hand of Ragnaros" {
+		item.decreaseQuality(1)
+		return
+	}
+	if item.Name == "Backstage passes to a TAFKAL80ETC concert" {
+		item.decreaseQuality(item.Quality)
+	}
 }
